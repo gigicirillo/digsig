96daa57feb22c2062c8329199d2395eddc90060a
gigicirillo <gigicirillo@gmail.com>
2026-09-15 21:47:21 +0200

    Allow one-shot screen playlists before persistent design

--- assets/js/design-exclusive.js
+++ assets/js/design-exclusive.js
@@ -3,18 +3,8 @@
   const originalResolve=resolve;
   resolve=function(c){
     originalResolve(c);
-    if(screen&&screen.designId&&(screen.useDesign!==false)){
-      playlists=[];
-      items=[];
-      index=0;
-    }
+    if(screen&&screen.designId&&(screen.useDesign!==false)&&screen.usePlaylists!==true){playlists=[];items=[];index=0}
   };
   const originalInactive=showInactive;
-  showInactive=function(){
-    if(screen&&screen.designId&&(screen.useDesign!==false)){
-      stage.style.background='#000';
-      return;
-    }
-    return originalInactive();
-  };
+  showInactive=function(){if(screen&&screen.designId&&(screen.useDesign!==false)){stage.style.background='#000';return}return originalInactive()};
 })();
